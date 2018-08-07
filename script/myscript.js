ee96a6b17b4423fafffd8b35d94842b3fce0eed0
Zach Wild <wild.zachary.d@gmail.com>
2018-08-07 16:09:33 -0400

    second push (lots of updates)

--- script/myscript.js
+++ script/myscript.js
@@ -6,3 +6,11 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
